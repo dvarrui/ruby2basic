f3c34a8bc59f1b4515765528c340e465216b24f8
David Vargas <dvarrui@protonmail.com>
2026-04-14 09:32:36 +0100

    test

--- test/transpiler/zxspectrum_test.rb
+++ test/transpiler/zxspectrum_test.rb
@@ -41,16 +41,8 @@ class TranspilerZXSpectrumTest < Test::Unit::TestCase
     output = '10 PRINT "Age: " + STR$(age) + "."'
     assert_equal(output, @r2b.transpile(input))
 
-    input = <<-INPUT
-      name = "Obiwan"
-      puts "Hello, #{name}!"
-    INPUT
-    output = <<-OUTPUT
-      10 LET jedi$ = "Obiwan"
-      20 PRINT "Hello, " + jedi$ + "!"
-      30 STOP
-    OUTPUT
-    # @r2b.oneline = false
-    # assert_equal(output, @r2b.transpile(input))
+    input = 'puts "Hello, #{name}!"'
+    output = '10 PRINT "Hello, " + STR$(name) + "!"'
+    assert_equal(output, @r2b.transpile(input))
   end
 end
